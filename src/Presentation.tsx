f319628bbd4cea3795875cb4159c26ecaba66f17
Zakary Chauve <chauve.zakary@sweetcom.ch>
2025-11-04 14:10:20 +0100

    image pour hero section et texte filler (à styliser)

--- src/Presentation.tsx
+++ src/Presentation.tsx
@@ -1,9 +1,24 @@
 import './App.css'
+import hero from './assets/hero.jpg'
 
 function Presentation() {
 
   return (
-    <></>
+    <>
+      <img src={hero} className='absolute h-[300px] w-screen top-[145px]'></img>
+
+      <p className='absolute top-[80%]'>
+        Le monde est en état d’urgence, à la veille d’un possible cataclysme
+        climatique, démographique, énergétique et financier. Biologique
+        également: espèce animale vivant parmi les autres, l'homme n'a guère
+        conscience de leur égalité de droit.
+        Bientôt une sagesse mondiale va émerger, dépouillée de toute attache
+        idéologique, de tout intégrisme religieux et fondée sur l'œuvre salvatrice
+        du Christ. Elle pourra s'appuyer sur une connexion planétaire via les
+        réseaux sociaux, qui pour l'heure servent souvent à des usages égoïstes,
+        déviés voire délictueux.
+      </p>
+    </>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,17 +3,17 @@ import '../index.css'
 function NavBar () {
     return (
             <>
-      <div class="relative w-screen h-[200px] p-0 m-0 text-black">
-        <div class=" top-0 l-[50%] text-3xl color-black pb-5">
+      <div className=" w-screen h-[125px] fixed top-0 p-0 m-0 text-black bg-white z-1">
+        <div className=" top-0 l-[50%] text-3xl color-black pb-5 pt-5">
           Charles Henri Hüssy
         </div>
-        <div class="absolute left l-[-50%] border-b-[1px] border-t-[1px] w-screen border-gray-500 space-x-15 p-3 caps">
-          <a href='/' class="hover:text-green-600">PRESENTATION</a>
-          <a href='/aPropos' class="hover:text-green-600">A PROPOS</a>
-          <a href='/Videos' class="hover:text-green-600">VIDEOS</a>
-          <a href='/Bibliographie' class="hover:text-green-600">BIBLIOGRAPHIE</a>
-          <a href='/Acheter' class="hover:text-green-600">ACHETER</a>
-          <a href='/Contact' class="hover:text-green-600">CONTACT</a>
+        <div className="absolute left l-[-50%] border-b-[1px] border-t-[1px] w-screen border-gray-500 space-x-15 p-3 caps">
+          <a href='/' className="hover:text-green-600">PRESENTATION</a>
+          <a href='/aPropos' className="hover:text-green-600">A PROPOS</a>
+          <a href='/Videos' className="hover:text-green-600">VIDEOS</a>
+          <a href='/Bibliographie' className="hover:text-green-600">BIBLIOGRAPHIE</a>
+          <a href='/Acheter' className="hover:text-green-600">ACHETER</a>
+          <a href='/Contact' className="hover:text-green-600">CONTACT</a>
         </div>
       </div>
     </>
