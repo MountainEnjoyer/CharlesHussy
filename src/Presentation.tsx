a290c7017fd4025ea7cb0f24052dd093d26e4beb
Zakary Chauve <chauve.zakary@sweetcom.ch>
2025-11-04 11:12:57 +0100

    starting to create route and components files

--- src/Presentation.tsx
+++ src/Presentation.tsx
@@ -3,8 +3,7 @@ import './App.css'
 function Presentation() {
 
   return (
-    <BrowserRouter>
-    </BrowserRouter>
+
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './Presentation.tsx'
 import NavBar from './components/NavBar.tsx'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Presentation from './Presentation.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
     <NavBar />
-    <App />
+
+    {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Presentation />} />
+        <Route path="/aPropos" element={<aPropos />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
