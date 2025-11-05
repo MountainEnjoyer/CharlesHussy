98abfc21cb401586da664b3207e5ac4f11ae5829
Zakary Chauve <chauve.zakary@sweetcom.ch>
2025-11-05 17:55:27 +0100

    Video finie

--- src/components/Propos.tsx
+++ src/components/Propos.tsx
@@ -4,12 +4,12 @@ import profile from "../assets/profile.jpg"
 function Propos() {
     return (
         <>
-            <div className=' absolute grid grid-cols-2 grid-rows-1 w-[80%] h-[100px] left-[100px] top-[175px]'>
-                <img src={profile} className='relative top-[-100px] scale-50 '></img>
-                <p className='relative text-xl text-black wrap  top-[100px]'>
+            <div className=' absolute grid grid-cols-3 grid-rows-1 w-[80%]  left-[100px] top-[160px] bg-gray-100 rounded-md'>
+                <img src={profile} className='relative rounded-md scale-75 '></img>
+                <p className='relative col-span-2 text-xl h-[25%] text-black wrap left-[-25px] top-[150px]'>
                     Charles Henri Hüssy, 84 ans, est professeur honoraire de l'université de
     Genève. Il a obtenu une licence en théologie, en sociologie, en
-    géographie, puis un do ctorat de géographie. Il a été Directeur d’un
+    géographie, puis un doctorat de géographie. Il a été Directeur d’un
     Centre Universitaire d’Écologie Humaine.
                 </p>
             </div>
--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -2,7 +2,9 @@ import '../index.css'
 
 function Videos() {
     return (
-        <>videos</>
+        <>
+            <iframe  className="relative left-[40%] bottom-[-65px]" width="700" height="393.75" src="https://www.youtube.com/embed/GHok9Y42mcA?si=R0i3x0gdMxGzmHpj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </>
     )
 }
 
