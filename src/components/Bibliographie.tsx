6931ff2a25725d603b1eed90321e77f25765ddcc
Zakary CHAUVE <chauve.zakary@gmail.com>
2025-11-07 14:11:17 +0100

    liste ajoutée à labibliographie

--- src/components/Bibliographie.tsx
+++ src/components/Bibliographie.tsx
@@ -3,7 +3,19 @@ import '../index.css'
 
 function Bibliographie() {
     return (
-        <>bibliographie</>
+        <>
+            <ul>
+                <li>Hüssy C.H., 1980. Genève, étude régionale. Essai d’analyse sémiologique en géographie humaine. Thèse No 272, Faculté des Sciences Économiques et Sociales, Berne, Francfort, Las Vegas, Peter Lang.</li>
+                <li>Hüssy C.H., 1988 Géographie politique, par Friedrich Ratzel., éd. Economica. Hüssy C.H., 1991. Atlas du Bassin Genevois et de la Région Lémanique. Un espace transfrontalier au cœur de l’Europe. Encyclopédie de Genève.</li>
+                <li>Hüssy C.H., 2016. Atlas du Grand Genève. État des lieux pour un progrès durable. Genève éd. Slatkine.</li>
+                <li>Hüssy C.H., 2018. Le Grand Genève, territoire hybride. Éditions universitaires européennes.</li>
+                <li>Hüssy C.H., 2021. L’ancienne alliance. Une modernité dans l’impasse ? Éd. L’Harmattan.</li>
+                <li>Hüssy C.H., 2021. Une Nouvelle Alliance. Nourrir une espérance pour l’après-effondrement. Éd. L’Harmattan.</li>
+                <li>Hüssy C.H., 2021. Un Christ vert. Un Dieu amoureux de sa Création. Éd. L’Harmattan.</li>
+                <li>Hüssy C.H., 2024, L’attente. En quête du projet de Dieu. Éd. L’Harmattan.</li>
+                <li>Hüssy C.H., 2026, Le mythe de Kairos. Changer ou disparaître. Éd. Saint-Augustin.</li>
+            </ul>
+        </>
     )
 }
 
