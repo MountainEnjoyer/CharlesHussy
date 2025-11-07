0ff42ee10e64cf5f74f9f77b61f3a2c20283080b
Zakary Chauve <chauve.zakary@sweetcom.ch>
2025-11-07 14:20:31 +0100

    test

--- src/components/Bibliographie.tsx
+++ src/components/Bibliographie.tsx
@@ -4,16 +4,16 @@ import '../index.css'
 function Bibliographie() {
     return (
         <>
-            <ul>
-                <li>Hüssy C.H., 1980. Genève, étude régionale. Essai d’analyse sémiologique en géographie humaine. Thèse No 272, Faculté des Sciences Économiques et Sociales, Berne, Francfort, Las Vegas, Peter Lang.</li>
-                <li>Hüssy C.H., 1988 Géographie politique, par Friedrich Ratzel., éd. Economica. Hüssy C.H., 1991. Atlas du Bassin Genevois et de la Région Lémanique. Un espace transfrontalier au cœur de l’Europe. Encyclopédie de Genève.</li>
-                <li>Hüssy C.H., 2016. Atlas du Grand Genève. État des lieux pour un progrès durable. Genève éd. Slatkine.</li>
-                <li>Hüssy C.H., 2018. Le Grand Genève, territoire hybride. Éditions universitaires européennes.</li>
-                <li>Hüssy C.H., 2021. L’ancienne alliance. Une modernité dans l’impasse ? Éd. L’Harmattan.</li>
-                <li>Hüssy C.H., 2021. Une Nouvelle Alliance. Nourrir une espérance pour l’après-effondrement. Éd. L’Harmattan.</li>
-                <li>Hüssy C.H., 2021. Un Christ vert. Un Dieu amoureux de sa Création. Éd. L’Harmattan.</li>
-                <li>Hüssy C.H., 2024, L’attente. En quête du projet de Dieu. Éd. L’Harmattan.</li>
-                <li>Hüssy C.H., 2026, Le mythe de Kairos. Changer ou disparaître. Éd. Saint-Augustin.</li>
+            <ul className='relative text-xl pt-[150px] w-[95%] pl-[7%]'>
+                <li>Hüssy C.H., 1980. Genève, étude régionale. Essai d’analyse sémiologique en géographie humaine. <br/>Thèse No 272, Faculté des Sciences Économiques et Sociales, Berne, Francfort, Las Vegas, Peter Lang.</li><br/>
+                <li>Hüssy C.H., 1988 Géographie politique, par Friedrich Ratzel., éd. Economica. Hüssy C.H., 1991. <br/>Atlas du Bassin Genevois et de la Région Lémanique. Un espace transfrontalier au cœur de l’Europe. Encyclopédie de Genève.</li><br/>
+                <li>Hüssy C.H., 2016. Atlas du Grand Genève. État des lieux pour un progrès durable. Genève éd. Slatkine.</li><br/>
+                <li>Hüssy C.H., 2018. Le Grand Genève, territoire hybride. Éditions universitaires européennes.</li><br/>
+                <li>Hüssy C.H., 2021. L’ancienne alliance. Une modernité dans l’impasse ? Éd. L’Harmattan.</li><br/>
+                <li>Hüssy C.H., 2021. Une Nouvelle Alliance. Nourrir une espérance pour l’après-effondrement. Éd. L’Harmattan.</li><br/>
+                <li>Hüssy C.H., 2021. Un Christ vert. Un Dieu amoureux de sa Création. Éd. L’Harmattan.</li><br/>
+                <li>Hüssy C.H., 2024, L’attente. En quête du projet de Dieu. Éd. L’Harmattan.</li><br/>
+                <li>Hüssy C.H., 2026, Le mythe de Kairos. Changer ou disparaître. Éd. Saint-Augustin.</li><br/>
             </ul>
         </>
     )
