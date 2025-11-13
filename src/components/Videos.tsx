cc1931e3b12fb2d114069b897d140a7513c214c6
Zakary Chauve <chauve.zakary@gmail.com>
2025-11-13 08:30:03 +0100

    test

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -3,7 +3,7 @@ import '../index.css'
 function Videos() {
     return (
         <>
-            <iframe  className="m-auto mt-[50%] lg:mt-2 w-[80%] h-1/3" src="https://www.youtube.com/embed/GHok9Y42mcA?si=R0i3x0gdMxGzmHpj" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <iframe  className="m-auto mt-[50%] lg:mt-2 w-3/3 h-3/3" src="https://www.youtube.com/embed/GHok9Y42mcA?si=R0i3x0gdMxGzmHpj" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </>
     )
 }
