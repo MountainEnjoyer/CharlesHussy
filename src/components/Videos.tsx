d44ef80af527364fef1a84638bebc21f4213b629
Zakary Chauve <chauve.zakary@gmail.com>
2025-11-13 08:41:45 +0100

    video in size and responsive; used IframeResizer

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,9 +1,19 @@
 import '../index.css'
+import IframeResizer from '@iframe-resizer/react'
+
 
 function Videos() {
     return (
         <>
-            <iframe  className="m-auto mt-[50%] lg:mt-2 w-3/3 h-3/3" src="https://www.youtube.com/embed/GHok9Y42mcA?si=R0i3x0gdMxGzmHpj" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <IframeResizer
+                license='GPLv3'
+                title="YouTube video player" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerPolicy="strict-origin-when-cross-origin" 
+                allowFullScreen
+                src="https://www.youtube.com/embed/GHok9Y42mcA?si=R0i3x0gdMxGzmHpj"
+                style={{ width: '70%',  height: '70vh', margin: "auto", marginTop: "2%" }}
+            />
         </>
     )
 }
