6e1922a9a4152e623a5a8bde3b4b06284bc49178
Zakary Chauve <chauve.zakary@gmail.com>
2025-11-13 17:29:05 +0100

    form en cours

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -3,11 +3,11 @@ import './index.css'
 function NavBar () {
     return (
     <>
-      <div className=" min-w-[100%] max-h-[10%]  top-0 p-0 m-auto text-black z-1">
-        <div className="w-min-fill text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl color-black pb-5 pt-5 bg-white z-1">
+      <div className=" min-w-[100%] top-0 p-0 m-auto text-black z-1">
+        <div className="w-min-fill text-md sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl 2xl:text-7xl color-black pb-5 pt-5 bg-white z-1">
           Charles Henri Hüssy
         </div>
-        <div className="flex gap-2 sm:gap-5 md:gap-15 lg:gap-25 xl:gap-30 py-1 flex-wrap place-content-center  border-b-[1px] border-t-[1px] h-max-[20%] min-w-[90%]  border-gray-500  text-[10px] sm:text-sm md:text-md lg:text-lg xl:text-2xl  bg-white z-1 ">
+        <div className="flex gap-2 sm:gap-4 md:gap-13 lg:gap-20 xl:gap-28 2xl:gap-40 py-1 flex-wrap place-content-center  border-b-[1px] border-t-[1px] h-max-[20%] min-w-[90%]  border-gray-500  text-[8px] sm:text-base md:text-sm lg:text-md xl:text-lg 2xl:text-3xl  bg-white z-1 ">
           <a href='/' className="hover:text-green-600 ">PRESENTATION</a>
           <a href='/apropos' className="hover:text-green-600 ">PROPOS</a>
           <a href='/Videos' className="hover:text-green-600 ">VIDEOS</a>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,19 +3,19 @@ import '../index.css'
 function Contact() {
     return (
         <>
-            <form action="" className='w-[50%] shadow-xs shadow-gray-400 rounded-sm p-3 m-auto mt-[2%]'>
-                <h2 className='text-2xl underline'>Formulaire de Contact</h2>
+            <form action="" className='w-[90%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] shadow-xs shadow-gray-400 rounded-sm p-3 m-auto mt-[2%]'>
+                <h2 className='text-[20px] sm:text-md md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-5xl m-auto underline'>Formulaire de Contact</h2>
                 <div className="input-box p-1">
-                    <label className='text-gray-700'>Nom Complet</label><br/>
-                    <input type='text' className=' w-[80%] border-1 border-gray-400 rounded-md p-1' placeholder='Entrez votre nom complet' required></input>
+                    <label className='text-gray-700 text-[12px] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl m-auto'>Nom Complet</label><br/>
+                    <input type='text' className='w-[99%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] border-1 border-gray-400 rounded-md p-1' placeholder='Entrez votre nom complet' required></input>
                 </div>
                 <div className="input-box p-1">
-                    <label className='text-gray-700 pt-3'>Adresse eMail</label><br/>
-                    <input type='text' className='w-[80%] border-1 border-gray-400 rounded-md p-1' placeholder='Entrez votre adresse email' required></input>
+                    <label className='text-gray-700 pt-3 text-[12px] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl m-auto'>Adresse eMail</label><br/>
+                    <input type='text' className='w-[99%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] border-1 border-gray-400 rounded-md p-1' placeholder='Entrez votre adresse email' required></input>
                 </div>
                 <div className="input-box p-1">
-                    <label className='text-gray-700 '>Message</label><br/>
-                    <textarea type='text' className='w-[80%] border-1 border-gray-400 rounded-md p-1' height="200px" placeholder='Entrez votre message' required></textarea>
+                    <label className='text-gray-700 text-[12px] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl m-auto '>Message</label><br/>
+                    <textarea type='text' className='w-[99%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] border-1 border-gray-400 rounded-md p-1' height="200px" placeholder='Entrez votre message' required></textarea>
                 </div>
                 <button type='submit' className='px-1 m-1 rounded-md shadow-none hover:shadow-2xs transition-shadow shadow-emerald-400'>Envoyer le message</button>
             </form>
