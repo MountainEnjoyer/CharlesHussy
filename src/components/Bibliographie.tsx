a03e9d1bb827dc27131d8669be4014f253b0b0b1
Zakary Chauve <chauve.zakary@gmail.com>
2025-11-13 10:06:31 +0100

    added the contact form gotta back end it

--- src/components/Bibliographie.tsx
+++ src/components/Bibliographie.tsx
@@ -4,7 +4,7 @@ import '../index.css'
 function Bibliographie() {
     return (
         <>
-            <ul className='text-[12px] sm:text-sm md:text-md lg:text-lg xl:text-2xl mt-[2%] '>
+            <ul className='text-[12px] sm:text-sm md:text-md lg:text-lg xl:text-2xl mt-[2%] text-wrap '>
                 <li><strong>Hüssy C.H., 1980. Genève, étude régionale. Essai d’analyse sémiologique en géographie humaine.</strong><br/>Thèse No 272, Faculté des Sciences Économiques et Sociales, Berne, Francfort, Las Vegas, Peter Lang.</li><br/>
                 <li><strong>Hüssy C.H., 1988 Géographie politique, par Friedrich Ratzel.,</strong> <br/>éd. Economica.</li><br/>
                 <li><strong>Hüssy C.H., 1991. Atlas du Bassin Genevois et de la Région Lémanique.</strong> <br/>Un espace transfrontalier au cœur de l’Europe. Encyclopédie de Genève.</li><br/>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,24 @@ import '../index.css'
 
 function Contact() {
     return (
-        <><p>contact</p></>
+        <>
+            <form action="" className='w-[50%] shadow-xs shadow-gray-400 rounded-sm p-3 m-auto mt-[2%]'>
+                <h2 className='text-2xl underline'>Formulaire de Contact</h2>
+                <div className="input-box p-1">
+                    <label className='text-gray-700'>Nom Complet</label><br/>
+                    <input type='text' className=' w-[80%] border-1 border-gray-400 rounded-md p-1' placeholder='Entrez votre nom complet' required></input>
+                </div>
+                <div className="input-box p-1">
+                    <label className='text-gray-700 pt-3'>Adresse eMail</label><br/>
+                    <input type='text' className='w-[80%] border-1 border-gray-400 rounded-md p-1' placeholder='Entrez votre adresse email' required></input>
+                </div>
+                <div className="input-box p-1">
+                    <label className='text-gray-700 '>Message</label><br/>
+                    <textarea type='text' className='w-[80%] border-1 border-gray-400 rounded-md p-1' height="200px" placeholder='Entrez votre message' required></textarea>
+                </div>
+                <button type='submit' className='px-1 m-1 rounded-md shadow-none hover:shadow-2xs transition-shadow shadow-emerald-400'>Envoyer le message</button>
+            </form>
+        </>
     )
 }
 
