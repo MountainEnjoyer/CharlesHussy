4e1c8945a93c549763a6c62df55035dcc3322fd2
Zakary Chauve <chauve.zakary@gmail.com>
2025-11-17 11:54:08 +0100

    Version finie et construite, prête au deploiement :)

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,29 +1,25 @@
 import '../index.css'
-import React, {useRef} from 'react';
-import emailjs from "@emailjs/browser"
+import {useRef} from 'react';
+import emailjs from "@emailjs/browser";
 
 function Contact() {
 
-    const form = useRef();
+    const formul = useRef();
 
-    const sendEmail = (e) => {
+    function sendEmail(e) {
         e.preventDefault();
-        emailjs.sendForm("service_z0gy6ef", "template_mcqn33x", form.current, "DwT7fucKEcpiVKRYi").then(
+        emailjs.sendForm("service_z0gy6ef", "template_mcqn33x", formul.current, "DwT7fucKEcpiVKRYi").then(
             () => {
-                emailjs.sendForm("service_z0gy6ef", "template_xm9x4xj", form.current, "DwT7fucKEcpiVKRYi")
+                emailjs.sendForm("service_z0gy6ef", "template_xm9x4xj", formul.current, "DwT7fucKEcpiVKRYi")
                 alert("Message envoyé avec succès, vous trouverez dans votre boite mail une confirmation.");
-                form.current.reset();
-            },
-            (error) => {
-                alert("Echec de l'envoie, merci de réessayer.", error);
+                formul.current.reset();
             }
-
         )
     }
 
     return (
         <>
-            <form ref={form} onSubmit={sendEmail} className='w-[90%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] shadow-xs shadow-gray-400 rounded-sm p-3 m-auto mt-[2%]'>
+            <form ref={formul} onSubmit={sendEmail} className='w-[90%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] shadow-xs shadow-gray-400 rounded-sm p-3 m-auto mt-[2%]'>
                 <h2 className='text-[20px] sm:text-md md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-5xl m-auto underline'>Formulaire de Contact</h2>
                 <div className="input-box p-1">
                     <label className='text-gray-700 text-[12px] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl m-auto'>Nom Complet</label><br/>
@@ -35,7 +31,7 @@ function Contact() {
                 </div>
                 <div className="input-box p-1">
                     <label className='text-gray-700 text-[12px] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl m-auto '>Message</label><br/>
-                    <textarea type='text' name="message" className='w-[99%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] border-1 border-gray-400 rounded-md p-1' height="200px" placeholder='Entrez votre message' required></textarea>
+                    <textarea name="message" className='w-[99%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] border-1 border-gray-400 rounded-md p-1'  required></textarea>
                 </div>
                 <button type='submit' className='px-1 m-1 rounded-md shadow-none hover:shadow-2xs transition-shadow shadow-emerald-400'>Envoyer le message</button>
             </form>
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import NavBar from './NavBar.tsx'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import Presentation from './components/Presentation.tsx'
-import Contact from './components/Contact.tsx'
+import Contact from './components/Contact.js'
 import Videos from './components/Videos.tsx'
 import Bibliographie from './components/Bibliographie.tsx'
 import Propos from "./components/Propos.tsx"
-import TestPropos from './components/TEST_Propos.tsx'
 import Acheter from './components/Acheter.tsx'
 import Footer from './Footer.tsx';
 
@@ -21,7 +20,6 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Presentation />} />
         <Route path="/apropos" element={<Propos />} />
-        <Route path="/testpropos" element={<TestPropos />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/acheter" element={<Acheter />} />
         <Route path="/videos" element={<Videos />} />
