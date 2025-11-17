9189cb4d37f51e3409e08bb8f0eaa9ac4c6675fe
Zakary Chauve <chauve.zakary@gmail.com>
2025-11-17 15:12:03 +0100

    texte

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -9,7 +9,7 @@ function NavBar () {
         </div>
         <div className="flex gap-2 sm:gap-4 md:gap-13 lg:gap-20 xl:gap-28 2xl:gap-40 py-1 flex-wrap place-content-center  border-b-[1px] border-t-[1px] h-max-[20%] min-w-[90%]  border-gray-500  text-[8px] sm:text-base md:text-sm lg:text-md xl:text-lg 2xl:text-3xl  bg-white z-1 ">
           <a href='/' className="hover:text-green-600 ">PRESENTATION</a>
-          <a href='/apropos' className="hover:text-green-600 ">PROPOS</a>
+          <a href='/apropos' className="hover:text-green-600 ">A PROPOS</a>
           <a href='/Videos' className="hover:text-green-600 ">VIDEOS</a>
           <a href='/Bibliographie' className="hover:text-green-600">BIBLIOGRAPHIE</a>
           <a href='/Acheter' className="hover:text-green-600 ">ACHETER</a>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,8 @@
 import '../index.css'
 import {useRef} from 'react';
 import emailjs from "@emailjs/browser";
+import { Bounce, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 function Contact() {
 
@@ -11,7 +13,17 @@ function Contact() {
         emailjs.sendForm("service_z0gy6ef", "template_mcqn33x", formul.current, "DwT7fucKEcpiVKRYi").then(
             () => {
                 emailjs.sendForm("service_z0gy6ef", "template_xm9x4xj", formul.current, "DwT7fucKEcpiVKRYi")
-                alert("Message envoyé avec succès, vous trouverez dans votre boite mail une confirmation.");
+                toast.success('Message envoyé avec succès. Vous trouverez dans votre boite mail une confirmation.', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Bounce,
+                });
                 formul.current.reset();
             }
         )
@@ -19,6 +31,19 @@ function Contact() {
 
     return (
         <>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Bounce}
+            />
             <form ref={formul} onSubmit={sendEmail} className='w-[90%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] shadow-xs shadow-gray-400 rounded-sm p-3 m-auto mt-[2%]'>
                 <h2 className='text-[20px] sm:text-md md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-5xl m-auto underline'>Formulaire de Contact</h2>
                 <div className="input-box p-1">
@@ -27,7 +52,7 @@ function Contact() {
                 </div>
                 <div className="input-box p-1">
                     <label className='text-gray-700 pt-3 text-[12px] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl m-auto'>Adresse eMail</label><br/>
-                    <input type='text' name="mail" className='w-[99%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] border-1 border-gray-400 rounded-md p-1' placeholder='Entrez votre adresse email' required></input>
+                    <input type='mail' name="mail" className='w-[99%] sm:w-[80%] md:w-[80%] lg:w-[80%] xl:w-[80%] border-1 border-gray-400 rounded-md p-1' placeholder='Entrez votre adresse email' required></input>
                 </div>
                 <div className="input-box p-1">
                     <label className='text-gray-700 text-[12px] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl m-auto '>Message</label><br/>
