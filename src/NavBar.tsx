ec2d571c43dbd0549cbfcfcaa4d2b0aefccf2d4d
Zakary Chauve <chauve.zakary@gmail.com>
2025-11-19 15:03:56 +0100

    React Router needs a <Link> tag to perfectly route the pages

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,4 +1,5 @@
 import './index.css'
+import { Link } from "react-router-dom";    
 
 function NavBar () {
     return (
@@ -8,12 +9,12 @@ function NavBar () {
           Charles Henri Hüssy
         </div>
         <div className="flex gap-4 sm:gap-4 md:gap-13 lg:gap-20 xl:gap-28 2xl:gap-40 py-1 flex-wrap place-content-center  border-b-[1px] border-t-[1px] h-max-[20%] min-w-[90%]  border-gray-500  text-[8px] sm:text-base md:text-sm lg:text-md xl:text-lg 2xl:text-3xl  bg-white z-1 ">
-          <a href='/' className="hover:text-green-600 ">PRESENTATION</a>
-          <a href='/apropos' className="hover:text-green-600 ">A PROPOS</a>
-          <a href='/Videos' className="hover:text-green-600 ">VIDEOS</a>
-          <a href='/Bibliographie' className="hover:text-green-600">BIBLIOGRAPHIE</a>
-          <a href='/Acheter' className="hover:text-green-600 ">ACHETER</a>
-          <a href='/Contact' className="hover:text-green-600">CONTACT</a>
+          <Link to="/" className="hover:text-green-600">PRESENTATION</Link>
+          <Link to="/a_propos" className="hover:text-green-600">A PROPOS</Link>
+          <Link to="/video" className="hover:text-green-600">VIDEO</Link>
+          <Link to="/bibliographie" className="hover:text-green-600">BIBLIOGRAPHIE</Link>
+          <Link to="/acheter" className="hover:text-green-600">ACHETER</Link>
+          <Link to="/contact" className="hover:text-green-600">CONTACT</Link>
         </div>
       </div>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar.tsx'
 import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import Presentation from './components/Presentation.tsx'
 import Contact from './components/Contact.js'
-import Videos from './components/Videos.tsx'
+import Video from './components/Videos.tsx'
 import Bibliographie from './components/Bibliographie.tsx'
 import Propos from "./components/Propos.tsx"
 import Acheter from './components/Acheter.tsx'
@@ -15,14 +15,13 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <NavBar />
-
     {/* Routes */}
       <Routes>
         <Route path="/" element={<Presentation />} />
-        <Route path="/apropos" element={<Propos />} />
+        <Route path="/a_propos" element={<Propos />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/acheter" element={<Acheter />} />
-        <Route path="/videos" element={<Videos />} />
+        <Route path="/video" element={<Video />} />
         <Route path="/bibliographie" element={<Bibliographie />} />
       </Routes>
     </BrowserRouter>
